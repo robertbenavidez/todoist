4379e83257a11281bdd077d0e00bbf85c3150b28
Robert Benavidez <murabitbenavidez@gmail.com>
2020-01-14 03:44:56 -0500

    intial boilerplate

---
+++ todoist/src/components/layout/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Header = () => {
+    const x = 1;
+
+    return (
+        <header className="header" data-testid='header'>
+            <nav>
+                <div className= 'logo'>
+                    <img src='/images/logo.png' alt='Todoist' />
+                </div>
+                <div className= 'settings'>
+                    <ul>
+                        <li>+</li>
+                        <li>Pizza Slice!</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
