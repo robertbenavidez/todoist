603afe4c40923e42f4851788f09714555242601f
Robert Benavidez <murabitbenavidez@gmail.com>
2020-01-15 09:05:00 -0500

    useProject hook

--- todoist/src/hooks/index.js
+++ todoist/src/hooks/index.js
@@ -44,8 +44,39 @@ export const useTask = selectedProject => {
             );
             setArchivedTasks(newTasks.filter(task => task.archived !== false)) 
         });
+
+        return () => unsubscribe()
     }, [selectedProject]);
+
+    return { tasks, archivedTasks}
 };
+
+
+
+
+export const useProjects = () => {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        firebase.firestore()
+        .collection('projects')
+        .where('userId', '==', '1')
+        .orderBy('projectId')
+        .get()
+        .then(snapshot => {
+            const allProjects = snapshot.docs.map(project => ({
+                ...project.data(),
+                docId: project.id,
+            }));
+
+            if (JSON.stringify(allProjects) !== JSON.stringify(projects)){
+                setProjects(allProjects)
+            }
+        })
+    }, [projects])
+
+    return {projects, setProjects};
+}
 
 
 
